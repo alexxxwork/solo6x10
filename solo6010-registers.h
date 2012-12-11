5b8fbc02b15ff64729527b9e933feacbf318c32d
Ismael Luceno <ismael.luceno@corp.bluecherry.net>
2012-12-11 10:03:00 -0300

    registers: Minor clean up for bit-fields from several registers

--- solo6010-registers.h
+++ solo6010-registers.h
@@ -217,21 +217,21 @@
 #define	SOLO_VI_ACT_I_P				0x011C
 #define	SOLO_VI_ACT_I_S				0x0120
 #define	SOLO_VI_ACT_P				0x0124
-#define	  SOLO_VI_FI_INVERT			(1<<31)
+#define	  SOLO_VI_FI_INVERT			(1 << 31)
 #define	  SOLO_VI_H_START(n)			((n)<<21)
 #define	  SOLO_VI_V_START(n)			((n)<<11)
 #define	  SOLO_VI_V_STOP(n)			((n)<<0)
 
 #define SOLO_VI_STATUS0				0x0128
-#define   SOLO_VI_STATUS0_PAGE(__n)		((__n) & 0x07)
+#define   SOLO_VI_STATUS0_PAGE(n)		((n) & 0x07)
 #define SOLO_VI_STATUS1				0x012C
 
 /* XXX: Might be better off in kernel level disp.h */
 #define DISP_PAGE(stat)				((stat) & 0x07)
 
 #define SOLO_VI_PB_CONFIG			0x0130
-#define	  SOLO_VI_PB_USER_MODE			(1<<1)
-#define	  SOLO_VI_PB_PAL			(1<<0)
+#define	  SOLO_VI_PB_USER_MODE			2
+#define	  SOLO_VI_PB_PAL			1
 #define SOLO_VI_PB_RANGE_HV			0x0134
 #define	  SOLO_VI_PB_HSIZE(h)			((h)<<12)
 #define	  SOLO_VI_PB_VSIZE(v)			((v)<<0)
@@ -273,20 +273,20 @@
 #define	SOLO_VI_MOT_CTRL			0x0264
 #define	  SOLO_VI_MOTION_FRAME_COUNT(n)		((n)<<24)
 #define	  SOLO_VI_MOTION_SAMPLE_LENGTH(n)	((n)<<16)
-#define	  SOLO_VI_MOTION_INTR_START_STOP	(1<<15)
-#define	  SOLO_VI_MOTION_FREEZE_DATA		(1<<14)
 #define	  SOLO_VI_MOTION_SAMPLE_COUNT(n)	((n)<<0)
+#define	  SOLO_VI_MOTION_INTR_START_STOP	(1 << 15)
+#define	  SOLO_VI_MOTION_FREEZE_DATA		(1 << 14)
 #define SOLO_VI_MOT_CLEAR			0x0268
 #define SOLO_VI_MOT_STATUS			0x026C
 #define	  SOLO_VI_MOTION_CNT(n)			((n)<<0)
 #define SOLO_VI_MOTION_BORDER			0x0270
 #define SOLO_VI_MOTION_BAR			0x0274
-#define	  SOLO_VI_MOTION_Y_SET			(1<<29)
-#define	  SOLO_VI_MOTION_Y_ADD			(1<<28)
-#define	  SOLO_VI_MOTION_CB_SET			(1<<27)
-#define	  SOLO_VI_MOTION_CB_ADD			(1<<26)
-#define	  SOLO_VI_MOTION_CR_SET			(1<<25)
-#define	  SOLO_VI_MOTION_CR_ADD			(1<<24)
+#define	  SOLO_VI_MOTION_Y_SET			(1 << 29)
+#define	  SOLO_VI_MOTION_Y_ADD			(1 << 28)
+#define	  SOLO_VI_MOTION_CB_SET			(1 << 27)
+#define	  SOLO_VI_MOTION_CB_ADD			(1 << 26)
+#define	  SOLO_VI_MOTION_CR_SET			(1 << 25)
+#define	  SOLO_VI_MOTION_CR_ADD			(1 << 24)
 #define	  SOLO_VI_MOTION_Y_VALUE(v)		((v)<<16)
 #define	  SOLO_VI_MOTION_CB_VALUE(v)		((v)<<8)
 #define	  SOLO_VI_MOTION_CR_VALUE(v)		((v)<<0)
@@ -295,16 +295,15 @@
  * Video Output
  */
 #define	SOLO_VO_FMT_ENC				0x0300
-#define	  SOLO_VO_SCAN_MODE_PROGRESSIVE		(1<<31)
-#define	  SOLO_VO_FMT_TYPE_PAL			(1<<30)
+#define	  SOLO_VO_SCAN_MODE_PROGRESSIVE		(1 << 31)
+#define	  SOLO_VO_FMT_TYPE_PAL			(1 << 30)
 #define   SOLO_VO_FMT_TYPE_NTSC			0
-#define	  SOLO_VO_USER_SET			(1<<29)
-
-#define	  SOLO_VO_FI_CHANGE			(1<<20)
-#define	  SOLO_VO_USER_COLOR_SET_VSYNC		(1<<19)
-#define	  SOLO_VO_USER_COLOR_SET_HSYNC		(1<<18)
-#define	  SOLO_VO_USER_COLOR_SET_NAH		(1<<17)
-#define	  SOLO_VO_USER_COLOR_SET_NAV		(1<<16)
+#define	  SOLO_VO_USER_SET			(1 << 29)
+#define	  SOLO_VO_FI_CHANGE			(1 << 20)
+#define	  SOLO_VO_USER_COLOR_SET_VSYNC		(1 << 19)
+#define	  SOLO_VO_USER_COLOR_SET_HSYNC		(1 << 18)
+#define	  SOLO_VO_USER_COLOR_SET_NAH		(1 << 17) /* Non Active of H. */
+#define	  SOLO_VO_USER_COLOR_SET_NAV		(1 << 16) /* Non Active of V. */
 #define	  SOLO_VO_NA_COLOR_Y(Y)			((Y)<<8)
 #define	  SOLO_VO_NA_COLOR_CB(CB)		(((CB)/16)<<4)
 #define	  SOLO_VO_NA_COLOR_CR(CR)		(((CR)/16)<<0)
@@ -320,32 +319,32 @@
 #define	  SOLO_VO_V_STOP(n)			((n)<<0)
 
 #define	SOLO_VO_RANGE_HV			0x030C
-#define	  SOLO_VO_SYNC_INVERT			(1<<24)
-#define	  SOLO_VO_HSYNC_INVERT			(1<<23)
-#define	  SOLO_VO_VSYNC_INVERT			(1<<22)
+#define	  SOLO_VO_SYNC_INVERT			(1 << 24)
+#define	  SOLO_VO_HSYNC_INVERT			(1 << 23)
+#define	  SOLO_VO_VSYNC_INVERT			(1 << 22)
 #define	  SOLO_VO_H_LEN(n)			((n)<<11)
 #define	  SOLO_VO_V_LEN(n)			((n)<<0)
 
 #define	SOLO_VO_DISP_CTRL			0x0310
-#define	  SOLO_VO_DISP_ON			(1<<31)
+#define	  SOLO_VO_DISP_ON			(1 << 31)
 #define	  SOLO_VO_DISP_ERASE_COUNT(n)		((n&0xf)<<24)
-#define	  SOLO_VO_DISP_DOUBLE_SCAN		(1<<22)
-#define	  SOLO_VO_DISP_SINGLE_PAGE		(1<<21)
 #define	  SOLO_VO_DISP_BASE(n)			(((n)>>16) & 0xffff)
+#define	  SOLO_VO_DISP_DOUBLE_SCAN		(1 << 22)
+#define	  SOLO_VO_DISP_SINGLE_PAGE		(1 << 21)
 
 #define SOLO_VO_DISP_ERASE			0x0314
 #define	  SOLO_VO_DISP_ERASE_ON			(1<<0)
 
 #define	SOLO_VO_ZOOM_CTRL			0x0318
-#define	  SOLO_VO_ZOOM_VER_ON			(1<<24)
-#define	  SOLO_VO_ZOOM_HOR_ON			(1<<23)
-#define	  SOLO_VO_ZOOM_V_COMP			(1<<22)
+#define	  SOLO_VO_ZOOM_VER_ON			(1 << 24)
+#define	  SOLO_VO_ZOOM_HOR_ON			(1 << 23)
+#define	  SOLO_VO_ZOOM_V_COMP			(1 << 22)
 #define	  SOLO_VO_ZOOM_SX(h)			(((h)/2)<<11)
 #define	  SOLO_VO_ZOOM_SY(v)			(((v)/2)<<0)
 
 #define SOLO_VO_FREEZE_CTRL			0x031C
-#define	  SOLO_VO_FREEZE_ON			(1<<1)
-#define	  SOLO_VO_FREEZE_INTERPOLATION		(1<<0)
+#define	  SOLO_VO_FREEZE_ON			(1 << 1)
+#define	  SOLO_VO_FREEZE_INTERPOLATION		(1 << 0)
 
 #define	SOLO_VO_BKG_COLOR			0x0320
 #define	  SOLO_BG_Y(y)				((y)<<16)
@@ -384,19 +383,19 @@
 #define SOLO_VO_EXPANSION(id)			(0x0250+((id)*4))
 
 #define	SOLO_OSG_CONFIG				0x03E0
-#define	  SOLO_VO_OSG_ON			(1<<31)
-#define	  SOLO_VO_OSG_COLOR_MUTE		(1<<28)
+#define	  SOLO_VO_OSG_ON			(1 << 31)
+#define	  SOLO_VO_OSG_COLOR_MUTE		(1 << 28)
 #define	  SOLO_VO_OSG_ALPHA_RATE(n)		((n)<<22)
 #define	  SOLO_VO_OSG_ALPHA_BG_RATE(n)		((n)<<16)
 #define	  SOLO_VO_OSG_BASE(offset)		(((offset)>>16)&0xffff)
 
 #define SOLO_OSG_ERASE				0x03E4
-#define	  SOLO_OSG_ERASE_ON			(0x80)
-#define	  SOLO_OSG_ERASE_OFF			(0x00)
+#define	  SOLO_OSG_ERASE_ON			(1 << 7)
+#define	  SOLO_OSG_ERASE_OFF			0
 
 #define SOLO_VO_OSG_BLINK			0x03E8
-#define	  SOLO_VO_OSG_BLINK_ON			(1<<1)
-#define	  SOLO_VO_OSG_BLINK_INTREVAL18		(1<<0)
+#define	  SOLO_VO_OSG_BLINK_ON			2
+#define	  SOLO_VO_OSG_BLINK_INTREVAL18		1
 
 #define SOLO_CAP_BASE				0x0400
 #define	  SOLO_CAP_MAX_PAGE(n)			((n)<<16)
@@ -424,7 +423,7 @@
 
 
 #define SOLO_VE_CFG0				0x0610
-#define	  SOLO_VE_TWO_PAGE_MODE			(1<<31)
+#define	  SOLO_VE_TWO_PAGE_MODE			(1 << 31)
 #define	  SOLO_VE_INTR_CTRL(n)			((n)<<24)
 #define	  SOLO_VE_BLOCK_SIZE(n)			((n)<<16)
 #define	  SOLO_VE_BLOCK_BASE(n)			((n)<<0)
@@ -444,8 +443,8 @@
 #define SOLO_VE_ENCRYP_POLY			0x0628
 #define SOLO_VE_ENCRYP_INIT			0x062C
 #define SOLO_VE_ATTR				0x0630
-#define	  SOLO_VE_LITTLE_ENDIAN			(1<<31)
-#define	  SOLO_COMP_ATTR_RN			(1<<30)
+#define	  SOLO_VE_LITTLE_ENDIAN			(1 << 31)
+#define	  SOLO_COMP_ATTR_RN			(1 << 30)
 #define	  SOLO_COMP_ATTR_FCODE(n)		((n)<<27)
 #define	  SOLO_COMP_TIME_INC(n)			((n)<<25)
 #define	  SOLO_COMP_TIME_WIDTH(n)		((n)<<21)
@@ -466,11 +465,11 @@
 #define SOLO_VE_OSD_BASE			0x0694
 #define SOLO_VE_OSD_CLR				0x0698
 #define SOLO_VE_OSD_OPT				0x069C
-#define   SOLO_VE_OSD_V_DOUBLE			(1<<16) /* 6110 Only */
-#define   SOLO_VE_OSD_H_SHADOW			(1<<15)
-#define   SOLO_VE_OSD_V_SHADOW			(1<<14)
-#define   SOLO_VE_OSD_H_OFFSET(n)		((n & 0x7f)<<7)
-#define   SOLO_VE_OSD_V_OFFSET(n)		(n & 0x7f)
+#define   SOLO_VE_OSD_V_DOUBLE			(1 << 16) /* 6110 Only */
+#define   SOLO_VE_OSD_H_SHADOW			(1 << 15)
+#define   SOLO_VE_OSD_V_SHADOW			(1 << 14)
+#define   SOLO_VE_OSD_H_OFFSET(n)		(((n) & 0x7f) << 7)
+#define   SOLO_VE_OSD_V_OFFSET(n)		((n) & 0x7f)
 
 #define SOLO_VE_CH_INTL(ch)			(0x0700+((ch)*4))
 #define SOLO_VE_CH_MOT(ch)			(0x0740+((ch)*4))
@@ -485,18 +484,18 @@
 #define SOLO_VE_JPEG_QUE(n)			(0x0A04+((n)*8))
 
 #define SOLO_VD_CFG0				0x0900
-#define	  SOLO_VD_CFG_NO_WRITE_NO_WINDOW	(1<<24)
-#define	  SOLO_VD_CFG_BUSY_WIAT_CODE		(1<<23)
-#define	  SOLO_VD_CFG_BUSY_WIAT_REF		(1<<22)
-#define	  SOLO_VD_CFG_BUSY_WIAT_RES		(1<<21)
-#define	  SOLO_VD_CFG_BUSY_WIAT_MS		(1<<20)
-#define	  SOLO_VD_CFG_SINGLE_MODE		(1<<18)
-#define	  SOLO_VD_CFG_SCAL_MANUAL		(1<<17)
-#define	  SOLO_VD_CFG_USER_PAGE_CTRL		(1<<16)
-#define	  SOLO_VD_CFG_LITTLE_ENDIAN		(1<<15)
-#define	  SOLO_VD_CFG_START_FI			(1<<14)
-#define	  SOLO_VD_CFG_ERR_LOCK			(1<<13)
-#define	  SOLO_VD_CFG_ERR_INT_ENA		(1<<12)
+#define	  SOLO_VD_CFG_NO_WRITE_NO_WINDOW	(1 << 24)
+#define	  SOLO_VD_CFG_BUSY_WIAT_CODE		(1 << 23)
+#define	  SOLO_VD_CFG_BUSY_WIAT_REF		(1 << 22)
+#define	  SOLO_VD_CFG_BUSY_WIAT_RES		(1 << 21)
+#define	  SOLO_VD_CFG_BUSY_WIAT_MS		(1 << 20)
+#define	  SOLO_VD_CFG_SINGLE_MODE		(1 << 18)
+#define	  SOLO_VD_CFG_SCAL_MANUAL		(1 << 17)
+#define	  SOLO_VD_CFG_USER_PAGE_CTRL		(1 << 16)
+#define	  SOLO_VD_CFG_LITTLE_ENDIAN		(1 << 15)
+#define	  SOLO_VD_CFG_START_FI			(1 << 14)
+#define	  SOLO_VD_CFG_ERR_LOCK			(1 << 13)
+#define	  SOLO_VD_CFG_ERR_INT_ENA		(1 << 12)
 #define	  SOLO_VD_CFG_TIME_WIDTH(n)		((n)<<8)
 #define	  SOLO_VD_CFG_DCT_INTERVAL(n)		((n)<<0)
 
@@ -509,39 +508,39 @@
 #define SOLO_VD_CODE_ADR			0x090C
 
 #define SOLO_VD_CTRL				0x0910
-#define	  SOLO_VD_OPER_ON			(1<<31)
+#define	  SOLO_VD_OPER_ON			(1 << 31)
 #define	  SOLO_VD_MAX_ITEM(n)			((n)<<0)
 
 #define SOLO_VD_STATUS0				0x0920
-#define	  SOLO_VD_STATUS0_INTR_ACK		(1<<22)
-#define	  SOLO_VD_STATUS0_INTR_EMPTY		(1<<21)
-#define	  SOLO_VD_STATUS0_INTR_ERR		(1<<20)
+#define	  SOLO_VD_STATUS0_INTR_ACK		(1 << 22)
+#define	  SOLO_VD_STATUS0_INTR_EMPTY		(1 << 21)
+#define	  SOLO_VD_STATUS0_INTR_ERR		(1 << 20)
 
 #define SOLO_VD_STATUS1				0x0924
 
 #define SOLO_VD_IDX0				0x0930
-#define	  SOLO_VD_IDX_INTERLACE			(1<<30)
+#define	  SOLO_VD_IDX_INTERLACE			(1 << 30)
 #define	  SOLO_VD_IDX_CHANNEL(n)		((n)<<24)
 #define	  SOLO_VD_IDX_SIZE(n)			((n)<<0)
 
 #define SOLO_VD_IDX1				0x0934
 #define	  SOLO_VD_IDX_SRC_SCALE(n)		((n)<<28)
 #define	  SOLO_VD_IDX_WINDOW(n)			((n)<<24)
-#define	  SOLO_VD_IDX_DEINTERLACE		(1<<16)
 #define	  SOLO_VD_IDX_H_BLOCK(n)		((n)<<8)
 #define	  SOLO_VD_IDX_V_BLOCK(n)		((n)<<0)
+#define	  SOLO_VD_IDX_DEINTERLACE		(1 << 16)
 
 #define SOLO_VD_IDX2				0x0938
-#define	  SOLO_VD_IDX_REF_BASE_SIDE		(1<<31)
-#define	  SOLO_VD_IDX_REF_BASE(n)		(((n)>>16)&0xffff)
+#define	  SOLO_VD_IDX_REF_BASE_SIDE		(1 << 31)
+#define	  SOLO_VD_IDX_REF_BASE(n)		(((n) >> 16) & 0xffff)
 
 #define SOLO_VD_IDX3				0x093C
 #define	  SOLO_VD_IDX_DISP_SCALE(n)		((n)<<28)
-#define	  SOLO_VD_IDX_INTERLACE_WR		(1<<27)
-#define	  SOLO_VD_IDX_INTERPOL			(1<<26)
-#define	  SOLO_VD_IDX_HOR2X			(1<<25)
 #define	  SOLO_VD_IDX_OFFSET_X(n)		((n)<<12)
 #define	  SOLO_VD_IDX_OFFSET_Y(n)		((n)<<0)
+#define	  SOLO_VD_IDX_INTERLACE_WR		(1 << 27)
+#define	  SOLO_VD_IDX_INTERPOL			(1 << 26)
+#define	  SOLO_VD_IDX_HOR2X			(1 << 25)
 
 #define SOLO_VD_IDX4				0x0940
 #define	  SOLO_VD_IDX_DEC_WR_PAGE(n)		((n)<<8)
@@ -561,21 +560,21 @@
 
 
 #define SOLO_IIC_CFG				0x0B20
-#define	  SOLO_IIC_ENABLE			(1<<8)
+#define	  SOLO_IIC_ENABLE			(1 << 8)
 #define	  SOLO_IIC_PRESCALE(n)			((n)<<0)
 
 #define SOLO_IIC_CTRL				0x0B24
-#define	  SOLO_IIC_AUTO_CLEAR			(1<<20)
-#define	  SOLO_IIC_STATE_RX_ACK			(1<<19)
-#define	  SOLO_IIC_STATE_BUSY			(1<<18)
-#define	  SOLO_IIC_STATE_SIG_ERR		(1<<17)
-#define	  SOLO_IIC_STATE_TRNS			(1<<16)
+#define	  SOLO_IIC_AUTO_CLEAR			(1 << 20)
+#define	  SOLO_IIC_STATE_RX_ACK			(1 << 19)
+#define	  SOLO_IIC_STATE_BUSY			(1 << 18)
+#define	  SOLO_IIC_STATE_SIG_ERR		(1 << 17)
+#define	  SOLO_IIC_STATE_TRNS			(1 << 16)
 #define	  SOLO_IIC_CH_SET(n)			((n)<<5)
-#define	  SOLO_IIC_ACK_EN			(1<<4)
-#define	  SOLO_IIC_START			(1<<3)
-#define	  SOLO_IIC_STOP				(1<<2)
-#define	  SOLO_IIC_READ				(1<<1)
-#define	  SOLO_IIC_WRITE			(1<<0)
+#define	  SOLO_IIC_ACK_EN			(1 << 4)
+#define	  SOLO_IIC_START			(1 << 3)
+#define	  SOLO_IIC_STOP				(1 << 2)
+#define	  SOLO_IIC_READ				(1 << 1)
+#define	  SOLO_IIC_WRITE			(1 << 0)
 
 #define SOLO_IIC_TXD				0x0B28
 #define SOLO_IIC_RXD				0x0B2C
@@ -642,19 +641,19 @@
 #define SOLO_TIMER_USEC_LSB			0x0d20 /* 6110 Only */
 
 #define SOLO_AUDIO_CONTROL			0x0D00
-#define	  SOLO_AUDIO_ENABLE			(1<<31)
-#define	  SOLO_AUDIO_MASTER_MODE		(1<<30)
-#define	  SOLO_AUDIO_I2S_MODE			(1<<29)
-#define	  SOLO_AUDIO_I2S_LR_SWAP		(1<<27)
-#define	  SOLO_AUDIO_I2S_8BIT			(1<<26)
 #define	  SOLO_AUDIO_I2S_MULTI(n)		((n)<<24)
-#define	  SOLO_AUDIO_MIX_9TO0			(1<<23)
 #define	  SOLO_AUDIO_DEC_9TO0_VOL(n)		((n)<<20)
-#define	  SOLO_AUDIO_MIX_19TO10			(1<<19)
 #define	  SOLO_AUDIO_DEC_19TO10_VOL(n)		((n)<<16)
 #define	  SOLO_AUDIO_MODE(n)			((n)<<0)
+#define	  SOLO_AUDIO_ENABLE			(1 << 31)
+#define	  SOLO_AUDIO_MASTER_MODE		(1 << 30)
+#define	  SOLO_AUDIO_I2S_MODE			(1 << 29)
+#define	  SOLO_AUDIO_I2S_LR_SWAP		(1 << 27)
+#define	  SOLO_AUDIO_I2S_8BIT			(1 << 26)
+#define	  SOLO_AUDIO_MIX_9TO0			(1 << 23)
+#define	  SOLO_AUDIO_MIX_19TO10			(1 << 19)
 #define SOLO_AUDIO_SAMPLE			0x0D04
-#define	  SOLO_AUDIO_EE_MODE_ON			(1<<30)
+#define	  SOLO_AUDIO_EE_MODE_ON			(1 << 30)
 #define	  SOLO_AUDIO_EE_ENC_CH(ch)		((ch)<<25)
 #define	  SOLO_AUDIO_BITRATE(n)			((n)<<16)
 #define	  SOLO_AUDIO_CLK_DIV(n)			((n)<<0)
